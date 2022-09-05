5324f2734ec6880ea5a722523339feb0c3a8a8b4
Gustavo Corone <gcoronelc@gmail.com>
2022-09-04 20:21:59 -0500

    Semana 2

---
+++ Semana01/GPO99TA01/Requerimiento1.sql
@@ -0,0 +1,38 @@
+/*
+Requerimiento 2
+Cada empleado recibe 3% de comisión de cada venta. 
+Se necesita una consulta que permita obtener la comisión que se le debe pagar a cada empleado por todas sus ventas.
+Base de datos: Northwind
+*/
+
+
+select o.EmployeeID, o.OrderID, d.ProductID, d.UnitPrice, d.Quantity
+from Northwind..Orders o
+join Northwind..[Order Details] d on o.OrderID=d.OrderID
+where o.EmployeeID=5
+order by 2,3
+
+
+
+
+
+
+with
+t1 as (
+	select o.EmployeeID, count(distinct o.OrderID) ventas,  sum(d.UnitPrice*d.Quantity) ImporteVentas 
+	from Northwind..Orders o
+	join Northwind..[Order Details] d on o.OrderID=d.OrderID
+	group by o.EmployeeID
+)
+select 
+	concat(e.FirstName,', ',e.LastName) nombre,
+	t1.ventas, t1.ImporteVentas, 
+	cast(t1.ImporteVentas*0.03 as numeric(10,2)) Comision
+from Northwind..Employees e
+join t1 on e.EmployeeID=t1.EmployeeID;
+go
+
+
+
+
+
