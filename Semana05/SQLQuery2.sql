46b5776fe2c069b8066cee96f2f697adce319015
Gustavo Corone <gcoronelc@gmail.com>
2022-10-02 08:18:45 -0500

    Taller 1

--- Semana05/SQLQuery2.sql
+++ Semana05/SQLQuery2.sql
@@ -12,4 +12,40 @@ GROUP BY d.nombre;
 GO
 
 
+-- Problema 3
+withv1 as (	select 		c.NomCurso "Nombre del Curso",		count(cp.IdCursoProg) "Aulas Programadas",		sum(cp.Vacantes+cp.Matriculados) "Vacantes Programadas",		sum(cp.Matriculados) "Matriculados"	from CursoProgramado cp	join Curso c on cp.IdCurso=c.IdCurso 	where cp.IdCiclo='2020-01'	group by c.NomCurso)select 	v1.*, v1.Matriculados*100.0/v1.[Vacantes Programadas]from v1
+go
+
+
+-- Problema 4
+select 	concat(p.NomProfesor,' ',p.ApeProfesor) "Nombre Profesor",	count(cp.IdCursoProg) "Aulas Programadas",	sum(t.Horas) "Horas Dictadas",	sum(t.PagoHora*t.Horas) "Importe"from CursoProgramado cpjoin Curso c on cp.IdCurso = c.IdCursojoin Tarifa t on c.IdTarifa = t.IdTarifajoin Profesor p on cp.IdProfesor = p.IdProfesorwhere cp.IdCiclo='2022-01'group by concat(p.NomProfesor,' ',p.ApeProfesor)
+go
+
+
+-- Problema 6
+
+WITH
+V1 AS (
+	SELECT 
+		O.ShipCountry "PAIS",
+		COUNT(DISTINCT O.OrderID) "PEDIDOS",
+		SUM(OD.Quantity) "CANTIDAD PRODUCTOS",
+		SUM(OD.Quantity*OD.UnitPrice) "IMPORTE"
+	FROM Northwind..[Order Details] OD 
+	JOIN Northwind..Orders O  ON OD.OrderID=O.OrderID
+	GROUP BY O.ShipCountry),
+V2 AS (SELECT SUM(V1.IMPORTE) TOTAL FROM V1)
+SELECT 
+	V1.*,
+	V1.IMPORTE*100.0/V2.TOTAL "PORCENTAJE"
+FROM V1 CROSS JOIN V2;
+GO
+
+SELECT SUM(Quantity) FROM Northwind..[Order Details];
+GO
+
+SELECT SUM(Quantity*UnitPrice) FROM Northwind..[Order Details];
+GO
+
+
 
